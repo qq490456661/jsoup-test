e7ad05a6955f65a7c82e68d5e4222ba602d4542a
linjunjie <linjunjie@raycloud.com>
2015-12-02 18:03:30 +0800

    first add

--- src/main/java/Hell.java
+++
@@ -1,7 +0,0 @@
-/**
- * Created by linjunjie on 2015/11/27 (linjunjie@raycloud.com).
- */
-public class Hell {
-
-    
-}
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,15 @@
 import org.htmlparser.Node;
+import org.htmlparser.NodeFilter;
 import org.htmlparser.Parser;
+import org.htmlparser.filters.TagNameFilter;
 import org.htmlparser.util.NodeIterator;
+import org.htmlparser.util.NodeList;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.*;
 
 
 /**
@@ -8,19 +17,82 @@ import org.htmlparser.util.NodeIterator;
  */
 public class Test {
     public static void main(String[]args){
-        System.out.println("hello world");
-        String url = "http://www.izero.tv/";
-        Parser parser = null;
+        String target = "http://www.izero.tv/list/kehuan/11";
+        Document document = null;
         try {
-
-            parser = new Parser(url);
-            parser.setEncoding("UTF-8");
-            NodeIterator iterator = parser.elements();
-            while(iterator.hasMoreNodes()){
-                Node node = iterator.nextNode();
-                System.out.println(node.toHtml());
-            }
+            //建立连接
+            document = Jsoup.connect(target)
+                    .timeout(30000)
+                    .get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // 容器
+        String str = "";
+        //选择<div class="oh">的内容
+        Elements elements = document.body().select("div.oh");
+        System.out.println("数量" + elements.size());
+        if(elements.size()==0){
+            return ; //结束爬取
+        }
+        // 获取<head>中内容
+        String temp = document.head().toString();
+        try {
+            str += temp;
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //截取<meta>中编码
+        int st = temp.indexOf("charset=");
+        st+=8;
+        int et = temp.indexOf("\"", st);
+        String encode = temp.substring(st,et);
+        System.out.println(encode);
+
+        //需要对链接进行处理
+        Elements a_links = elements.select("a");
+        for(Element e : a_links){
+            e.attr("href",e.attr("abs:href"));
+        }
+        //拼接成json
+        StringBuilder json = new StringBuilder("[");
+        Elements li_nodes = elements.select("ul").get(0).select("li");
+        String[] dataType = new String[]{"","title","type","clickNum"};
+        if(li_nodes!=null){
+            for(int k=0;k<li_nodes.size();k++){
+                Element e = li_nodes.get(k);
+                json.append("{");
+                Elements es = e.children();
+                e.children();
+                if(es!=null){
+                    for(int i=1;i<es.size();i++){
+                        String t = es.get(i).text();
+                        String[] srr = t.split("[:：]");
+                        if(srr.length>1){
+                            t = srr[1];
+                        }else{
+                            t = srr[0];
+                        }
+                        json.append("\""+dataType[i]+"\":\""+t+"\"");
+                        if(i<es.size()-1){
+                            json.append(",");
+                        }
+                    }
+                    json.append("}");
+                    if(k<li_nodes.size()-1){
+                        json.append(",\r\n");
+                    }
+                }
+            }
+        }
+        json.append("]");
+        System.out.println(json);
+        str += elements.toString();
+        //输出到文件中
+        try {
+            OutputStream os = new FileOutputStream(new File("D://1.html"));
+            os.write(str.getBytes(encode));
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
